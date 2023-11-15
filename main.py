f581dc103ca998068617d9193018262c692a1855
Omar Al Itani <omar.h.itani73@gmail.com>
2023-11-15 15:51:23 +0200

    commit

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ reads_file = './input/reads1.txt'
 reads = db.read_reads(reads_file)
 
 # Construct De Bruijn Graph
-k = 40
-g = db.construct_graph(reads, 31)
+k = 31
+g = db.construct_graph(reads, k)
 
 # Build the Genome
 contig = db.output_contigs(g)
